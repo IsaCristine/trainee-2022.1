e0e784cc68ef1635a54914a9edefe0bdd2b1eb1c
IsaCristine <bell2011jf@gmail.com>
2022-05-14 16:20:41 -0300

    Insere comentários em routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,63 @@
 <?php
 
+/*EXEMPLO DE ROTA -------------------------------------------------------------------
+
+$router->method('exemple', 'ExempleController@Exemple');
+
+FIM DO EXEMPLO --------------------------------------------------------------------*/
+
+
+
+//Rotas Usuários
+
+//Fim Usuários
+
+
+
+//Rotas Produtos
+
+//Fim Produtos
+
+
+
+//Rotas Categorias
+
+//Fim Categorias
+
+
+
+//Rotas Home
+
+//Fim Home
+
+
+
+//Rotas Quem Somos
+
+//Fim Quem Somos
+
+
+
+//Rotas Contato
+
+//Fim Contato
+
+
+
+//Rotas Produto
+
+//Fim Produto
+
+
+
+//Rotas Login
+
+//Fim Login
+
+
+
+//Rotas Dashboard
+
+//Fim Dashboard
+
 ?>

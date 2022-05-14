bdc83de11e9d812fa9d112249fe6c8d46dfc8002
IsaCristine <bell2011jf@gmail.com>
2022-05-14 17:06:41 -0300

    Muda os arquivos .html para .php

--- app/controllers/ExampleController.php
+++ app/controllers/ExampleController.php
@@ -7,36 +7,43 @@ use Exception;
 
 class ExampleController
 {
+    //Listar todos os registros:
     public function index()
     {
         
     }
 
+    //Exibição um registro:
     public function show()
     {
 
     }
 
+    //Criação de um registro:
     public function create()
     {
  
     }
 
+    //Armazenar um registro:
     public function store()
     {
 
     }
 
+    //Editar um registro:
     public function edit()
     {
   
     }
 
+    //Atualizar um registro:
     public function update()
     {
         
     }
 
+    //Deletar um registro:
     public function delete()
     {
  

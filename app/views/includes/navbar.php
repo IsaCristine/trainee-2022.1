958f1f58b90173ebc9682c0668a2a4c983e61413
Kaymartins <kayanmartins1@gmail.com>
2022-05-19 23:58:05 -0300

    Adiciona navbar,footer,sidebar na pasta includes e arruma codigo sidebar

---
+++ app/views/includes/navbar.php
@@ -0,0 +1,34 @@
+<header>
+    <div class="container" id="nav-container">
+
+        <nav class="navbar navbar-expand-lg fixed-top navbar-dark text-white">
+
+            <div class="container-fluid">
+                <!-- Logo -->
+                <a href="#" class="navbar-brand">
+                    <img src="../../public/img/Logo escrita amarela com flecha.png" alt="Logo Bell Comics" id="logo">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="navbar-collapse collapse justify-content-end" id="navbarToggleExternalContent">
+
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="#" class="nav-item nav-link">Home</a></li>
+                        <li class="nav-item"><a href="#" class="nav-item nav-link">Produtos</a></li>
+                        <li class="nav-item"><a href="#" class="nav-item nav-link">Quem somos</a></li>
+                        <li class="nav-item"><a href="#" class="nav-item nav-link">Contato</a></li>
+                        <li class="nav-item"><a href="#" class="nav-item nav-link">Login</a></li>
+                    </ul>
+
+                </div>
+
+            </div>
+        </nav>
+
+    </div>
+</header>

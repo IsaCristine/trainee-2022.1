f7e4bfbd95c8ad9199f8b0e6855783f947166fde
“GuilhermeMonteir0git config --global user.name “GuilhermeMonteir0 <mamacode@codejr.com.br>
2022-05-26 16:52:00 -0300

    Criando branch

---
+++ app/controllers/ProdutosController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class ExampleController
+{
+    //Renderiza a página para listar todos os registros:
+    public function index()
+    {
+        
+    }
+
+    //Renderiza a página para exibir um registro:
+    public function show()
+    {
+
+    }
+
+    //Renderiza a página para criar de um registro:
+    public function create()
+    {
+ 
+    }
+
+    //Renderiza a página para armazenar um registro:
+    public function store()
+    {
+
+    }
+
+    //Renderiza a página para editar um registro:
+    public function edit()
+    {
+  
+    }
+
+    //Renderiza a página para atualizar um registro:
+    public function update()
+    {
+        
+    }
+
+    //Renderiza a página para deletar um registro:
+    public function delete()
+    {
+ 
+    }
+}

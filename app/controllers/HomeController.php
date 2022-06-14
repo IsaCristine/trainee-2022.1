a1cadcf5b071e19c2cdeeb43b2b2997295b81f95
Kaymartins <kayanmartins1@gmail.com>
2022-06-14 13:52:53 -0300

    implementa back_end home

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controllers;
 
-class HomeController
+use App\Libs\Pagination;
+
+class HomeController extends Pagination
 {
     public function getPage():void
     {
+        $produtos = $this->list_products(0,8);
         require __DIR__ . '/../views/site/view_home.view.php';
     }
 }
